6a71d32746057652216e4113fe40d049f10c893d
Timotej Milčák <timotejmk94@gmail.com>
2025-12-28 00:22:35 +0100

    Refactor + implement presets/profiles

--- export_as_array.py
+++ export_as_array.py
@@ -32,16 +32,70 @@ import bpy
 import bmesh
 from bpy.props import *
 
+class Profile():
+    def __init__(self,
+                 name,
+                 vert_suffix, index_suffix,
+                 array_begin, array_delim, array_end,
+                 object_begin, object_delim, object_end):
+        self.name = name
+        self.vert_suffix = vert_suffix
+        self.index_suffix = index_suffix
+        self.array_begin = array_begin
+        self.array_delim = array_delim
+        self.array_end = array_end
+        self.object_begin = object_begin
+        self.object_delim = object_delim
+        self.object_end = object_end
+
+profiles = {
+    "profile_empty":Profile("Empty", "", "", "", "", "", "", "", ""),
+    "profile_c": Profile("C", "f", "", "{", ",", "};", "{", ",", "}"),
+    "profile_py": Profile("Python", "", "", "[", ",", "]", "[", ",", "]")
+}
+
+def generate_profile_items(self, context):
+    result = []
+    for key, value in profiles.items():
+        result.append((key, value.name, ""))
+    return result
+
+def change_profile(self, context):
+    changed_to = self.profiles_enum
+    storage = bpy.context.scene.export_as_array_properties
+
+    storage.vert_suffix = profiles[changed_to].vert_suffix
+    storage.index_suffix = profiles[changed_to].index_suffix
+    storage.array_begin = profiles[changed_to].array_begin
+    storage.array_delim = profiles[changed_to].array_delim
+    storage.array_end = profiles[changed_to].array_end
+    storage.object_begin = profiles[changed_to].object_begin
+    storage.object_delim = profiles[changed_to].object_delim
+    storage.object_end = profiles[changed_to].object_end
+
+class ExportAsArrayProperties(bpy.types.PropertyGroup):
+    vert_suffix: StringProperty()
+    index_suffix: StringProperty()
+    array_begin: StringProperty()
+    array_delim: StringProperty()
+    array_end: StringProperty()
+    object_begin: StringProperty()
+    object_delim: StringProperty()
+    object_end: StringProperty()
+    include_faces: BoolProperty(default=True)
+
 def get_array_string_for(mesh):
-    vert_suffix = bpy.context.scene.option_vert_suffix
-    index_suffix = bpy.context.scene.option_index_suffix
-    array_begin = bpy.context.scene.option_array_begin
-    array_delim = bpy.context.scene.option_array_delim
-    array_end = bpy.context.scene.option_array_end
-    object_begin = bpy.context.scene.option_object_begin
-    object_delim = bpy.context.scene.option_object_delim
-    object_end = bpy.context.scene.option_object_end
-    include_faces = bpy.context.scene.option_include_faces
+    storage = bpy.context.scene.export_as_array_properties
+
+    vert_suffix = storage.vert_suffix
+    index_suffix = storage.index_suffix
+    array_begin = storage.array_begin
+    array_delim = storage.array_delim
+    array_end = storage.array_end
+    object_begin = storage.object_begin
+    object_delim = storage.object_delim
+    object_end = storage.object_end
+    include_faces = storage.include_faces
 
     result = "verts = "
     result += array_begin
@@ -113,7 +167,7 @@ class OBJECT_OT_export_as_array_disk(bpy.types.Operator):
         return {"RUNNING_MODAL"};
 
     def execute(self, context):
-        self.report({'ERROR'}, "UNIMPLEMENTED!")
+        self.report({"ERROR"}, "UNIMPLEMENTED!")
         return {"FINISHED"}
 
 class OBJECT_OT_export_as_array_clipboard(bpy.types.Operator):
@@ -128,54 +182,65 @@ class OBJECT_OT_export_as_array_clipboard(bpy.types.Operator):
             if active_object is not None and active_object.type == "MESH":
                 string = get_array_string_for(active_object.to_mesh())
                 context.window_manager.clipboard = string
+                self.report({"INFO"}, "Successfuly copied")
             else:
-                self.report({'ERROR'}, "Invalid object selected!")
+                self.report({"ERROR"}, "Invalid object selected!")
         else:
-            self.report({'ERROR'}, "Select precisely one object!")
+            self.report({"ERROR"}, "Select precisely one object!")
         return {"FINISHED"}
 
 class OBJECT_OT_export_as_array(bpy.types.Operator):
     bl_label = "Export as array"
     bl_idname = "object.export_as_array"
 
+    profiles_enum: EnumProperty(
+        name="Profile",
+        update=change_profile,
+        items=generate_profile_items,
+    )
+
     def invoke(self, context, event):
         return context.window_manager.invoke_popup(self, width=250)
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Export settings")
+        row = layout.row()
+        row.label(text="Export settings")
+        row.prop(self, "profiles_enum")
         layout.separator(type="LINE")
 
         split = layout.split(factor=0.5)
         left_col = split.column()
         right_col = split.column()
 
+        storage = bpy.context.scene.export_as_array_properties
+
         left_col.label(text="Vertex suffix")
-        right_col.prop(bpy.context.scene, "option_vert_suffix", text="")
+        right_col.prop(storage, "vert_suffix", text="")
 
         left_col.label(text="Index suffix")
-        right_col.prop(bpy.context.scene, "option_index_suffix", text="")
+        right_col.prop(storage, "index_suffix", text="")
 
         left_col.label(text="Array begin")
-        right_col.prop(bpy.context.scene, "option_array_begin", text="")
+        right_col.prop(storage, "array_begin", text="")
 
         left_col.label(text="Array delimiter")
-        right_col.prop(bpy.context.scene, "option_array_delim", text="")
+        right_col.prop(storage, "array_delim", text="")
 
         left_col.label(text="Array end")
-        right_col.prop(bpy.context.scene, "option_array_end", text="")
+        right_col.prop(storage, "array_end", text="")
 
         left_col.label(text="Object begin")
-        right_col.prop(bpy.context.scene, "option_object_begin", text="")
+        right_col.prop(storage, "object_begin", text="")
 
         left_col.label(text="Object delimiter")
-        right_col.prop(bpy.context.scene, "option_object_delim", text="")
+        right_col.prop(storage, "object_delim", text="")
 
         left_col.label(text="Object end")
-        right_col.prop(bpy.context.scene, "option_object_end", text="")
+        right_col.prop(storage, "object_end", text="")
 
         left_col.label(text="Include faces")
-        right_col.prop(bpy.context.scene, "option_include_faces", text="")
+        right_col.prop(storage, "include_faces", text="")
 
         layout.operator(OBJECT_OT_export_as_array_clipboard.bl_idname)
         layout.operator(OBJECT_OT_export_as_array_disk.bl_idname)
@@ -189,35 +254,16 @@ def menu_func(self, context):
     layout.operator(OBJECT_OT_export_as_array.bl_idname)
 
 def register():
-    bpy.types.Scene.option_vert_suffix = StringProperty(default="f")
-    bpy.types.Scene.option_index_suffix = StringProperty(default="")
-
-    bpy.types.Scene.option_array_begin = StringProperty(default="{")
-    bpy.types.Scene.option_array_delim = StringProperty(default=",")
-    bpy.types.Scene.option_array_end = StringProperty(default="};")
-
-    bpy.types.Scene.option_object_begin = StringProperty(default="{")
-    bpy.types.Scene.option_object_delim = StringProperty(default=",")
-    bpy.types.Scene.option_object_end = StringProperty(default="}")
-
-    bpy.types.Scene.option_include_faces = BoolProperty(default=True)
-
+    bpy.utils.register_class(ExportAsArrayProperties)
+    bpy.types.Scene.export_as_array_properties = PointerProperty(type=ExportAsArrayProperties)
     bpy.utils.register_class(OBJECT_OT_export_as_array)
     bpy.utils.register_class(OBJECT_OT_export_as_array_clipboard)
     bpy.utils.register_class(OBJECT_OT_export_as_array_disk)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
-    del bpy.types.Scene.option_vert_suffix
-    del bpy.types.Scene.option_index_suffix
-    del bpy.types.Scene.option_array_begin
-    del bpy.types.Scene.option_array_delim
-    del bpy.types.Scene.option_array_end
-    del bpy.types.Scene.option_object_begin
-    del bpy.types.Scene.option_object_delim
-    del bpy.types.Scene.option_object_end
-    del bpy.types.Scene.option_include_faces
-
+    del bpy.types.Scene.export_as_array_properties
+    bpy.utils.unregister_class(ExportAsArrayProperties)
     bpy.utils.unregister_class(OBJECT_OT_export_as_array)
     bpy.utils.unregister_class(OBJECT_OT_export_as_array_clipboard)
     bpy.utils.unregister_class(OBJECT_OT_export_as_array_disk)
